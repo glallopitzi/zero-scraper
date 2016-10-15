d6f4bac8179cec7cfbfd7e9d3f898741f4c480dd
gianc <giancarlolallopizzi@gmail.com>
2016-10-15 20:55:32 +0200

    begin implementation for ES management

--- zero-scraper.py
+++ zero-scraper.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 
 import scrapy
@@ -7,6 +8,17 @@ from scrapy.crawler import CrawlerProcess
 from worker.spiders.base_spider import BaseSpider
 
 
+parser = argparse.ArgumentParser(description='Zero scraper!')
+parser.add_argument("--a")
+args = parser.parse_args()
+
+if args.a == 'create':
+    es = Elasticsearch([{'host': 'local.docker.dev'}])
+    print es.indices.create(index='scrapy', ignore=400)
+
+if args.a == 'delete':
+    es = Elasticsearch([{'host': 'local.docker.dev'}])
+    print es.indices.delete("scrapy")
 
 
 
@@ -21,17 +33,21 @@ from worker.spiders.base_spider import BaseSpider
 # process.start() # the script will block here until the crawling is finished
 
 
-es = Elasticsearch([
-    {'host': '192.168.99.100'}
-])
 
-print es.info()
-es.indices.delete("scrapy")
 
-# print es.indices.exists(index='scrapy')
-# print es.count(index='scrapy')
+# print es.info()
 
-# res = es.search(index='scrapy', q='trilocale')
-# print res['total']
-# print json.dumps(res)
-# print json.dumps(es.indices.analyze(index='scrapy'))
+
+# # print es.indices.exists(index='scrapy')
+# # print es.count(index='scrapy')
+# already_seen_url = []
+# res = es.search(index='scrapy', filter_path=['hits.hits._source.url'], q='website:subito.it', size=1000)
+# for elem in res['hits']['hits']:
+#     already_seen_url.append(elem['_source']['url'])
+#
+# # res_string = json.dumps(res)
+# # res_obj = json.loads(res_string)
+# # print res_obj['hits']
+# # for url in res_obj['hits']['hits']:
+# #     print url
+# # print json.dumps(es.indices.analyze(index='scrapy'))
