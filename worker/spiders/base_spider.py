d3ac9a92f625093266b94cab580cd9f95ad430eb
gianc <giancarlolallopizzi@gmail.com>
2016-11-06 16:18:09 +0100

    begin to differentiate spiders respect to crawled objects

--- worker/spiders/base_spider.py
+++ worker/spiders/base_spider.py
@@ -20,17 +20,6 @@ class BaseSpider(scrapy.Spider):
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',
     }
 
-    def __init__(self, name=None, category=None, region=None, ads_type=None, city=None, *args, **kwargs):
-        super(BaseSpider, self).__init__(*args, **kwargs)
-
-        self.args = dict(name=name, category=category, region=region, ads_type=ads_type, city=city)
-
-        self.name = name
-        self.load_config(name)
-        self.allowed_domains = [self.parser.get('general', 'allowed_domains')]
-        self.get_already_seen_urls()
-        self.start_urls = [Template(self.parser.get('general', 'start_urls')).substitute(self.args)]
-
     def parse(self, response, next_flag=True):
         self.logger.info('A response from %s just arrived!', response.url)
         ad_urls = response.xpath(self.parser.get('general', 'items_list')).extract()
@@ -48,7 +37,6 @@ class BaseSpider(scrapy.Spider):
                 self.logger.debug("URL %s already crawled" % url_string)
 
     def parse_ads(self, response):
-
         to_add = {
             'url': response.url,
             'website': self.parser.get('general', 'allowed_domains'),
@@ -59,11 +47,11 @@ class BaseSpider(scrapy.Spider):
 
         yield Ad(to_add)
 
-    def load_config(self, name):
+    def load_config(self, spider_type, name):
         self.parser = RawConfigParser()
-        config_filename = "config/%s.cfg" % name
+        config_filename = "config/%s/%s.cfg" % (spider_type, name)
         self.parser.read(config_filename)
-        self.logger.info('Config for %s loaded from %s, found %s sections' % (config_filename, self.name, len(self.parser.sections())))
+        self.logger.info('Config %s for type %s loaded from %s, found %s sections' % (config_filename, spider_type, self.name, len(self.parser.sections())))
 
     def extract_field(self, response, name):
         res = ""
@@ -77,7 +65,8 @@ class BaseSpider(scrapy.Spider):
             print sys.exc_info()
             return ""
 
-    def get_absolute_url_string(self, url, response):
+    @staticmethod
+    def get_absolute_url_string(url, response):
         url_string = response.urljoin(url)
         return url_string
 
@@ -92,3 +81,6 @@ class BaseSpider(scrapy.Spider):
         if res:
             for elem in res['hits']['hits']:
                 self.already_seen_urls.append(elem['_source']['url'])
+
+    def get_start_urls_from_template(self):
+        return Template(self.parser.get('general', 'start_urls')).substitute(self.args)
---
+++ worker/spiders/generic_spider.py
@@ -0,0 +1,25 @@
+from worker.spiders.base_spider import BaseSpider
+
+
+class GenericSpider(BaseSpider):
+
+    parser = None
+    name = 'generic_spider'
+    args = None
+    max_pages = 0
+    already_seen_urls = []
+
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',
+    }
+
+    def __init__(self, name=None, category=None, region=None, ads_type=None, city=None, *args, **kwargs):
+        super(BaseSpider, self).__init__(*args, **kwargs)
+
+        self.args = dict(name=name, category=category, region=region, ads_type=ads_type, city=city)
+
+        self.name = name
+        self.load_config('home', name)
+        self.allowed_domains = [self.parser.get('general', 'allowed_domains')]
+        self.get_already_seen_urls()
+        self.start_urls = [self.get_start_urls_from_template()]
